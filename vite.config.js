082a48c4b98b74c9cab04dc44d3c2a22f19b1d91
monupal1122 <monupalji1122@gmail.com>
2025-12-27 16:30:48 +0530

    update congif vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true, // allow external host
+    allowedHosts: ['freshmart-6whw.onrender.com']
+  }
 })
